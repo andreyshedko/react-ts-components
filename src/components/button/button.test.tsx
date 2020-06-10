28df17f06ec8d2bac32e48a5321b12e6d6ceeae1
andreyshedko <andrey.shedko@gmail.com>
2020-06-10 21:00:25 +0200

    Add test

---
+++ src/components/button/button.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import { ButtonComponent, ButtonComponentProps } from "./button.component";
+
+describe("Button Component", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    // ???
+  });
+}); 
